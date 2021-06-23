c2a24946844bd1b2dac89a7e57d7239530a7a995
doga <ydoga123@gmail.com>
2021-06-24 01:27:50 +0300

    Cleaned up Move On Click script and the player prefab.

--- Assets/Scripts/MoveOnClick.cs
+++ Assets/Scripts/MoveOnClick.cs
@@ -5,49 +5,62 @@ public class MoveOnClick : MonoBehaviour
 {
     [Header("Movement")]
     [SerializeField] private float speed;
+    [SerializeField] private bool useGravity;
+    [SerializeField] private float gravity;
     [Header("References")]
     [SerializeField] private Camera cam;
     [SerializeField] private CharacterController controller;
-    [SerializeField] private LayerMask groundLayer;
     [SerializeField] private GameObject selectionPrefab;
+    [SerializeField] private LayerMask groundLayer;
 
-    private Transform target;
+    private Transform cursor;
     private List<GameObject> toDestroy = new List<GameObject>();
+
+    private float verticalSpeed = 0f;
+
+    private void Awake()
+    {
+        cursor = Instantiate(selectionPrefab).GetComponent<Transform>();
+        cursor.gameObject.SetActive(false);
+    }
 
     private void Update()
     {
-        CheckInput();
+        MoveCursor();
+        Vector3 move = Vector3.zero;
 
-        if (target)
+        if (cursor && Input.GetMouseButton(0))
         {
-            Vector3 dir = target.position - transform.position;
-            controller.Move(dir * speed * Time.deltaTime);
+            Vector3 dir = cursor.position - transform.position;
+            dir = new Vector3(dir.x, 0, dir.z);
+            move += dir * speed * Time.deltaTime;
         }
 
-        if (Time.frameCount % 10 == 0)
+        if (useGravity)
         {
-            Clean();
+            if (Physics.CheckSphere(transform.position, 0.2f)) 
+                verticalSpeed = 0f;
+            else
+                Debug.Log("I am not grounded");
+
+            verticalSpeed += gravity  * Time.deltaTime;
         }
+
+        controller.Move(move + Vector3.up * verticalSpeed * Time.deltaTime);
     }
 
-    private void CheckInput()
+    private void MoveCursor()
     {
-        if (Input.GetMouseButtonDown(0))
+        RaycastHit hitInfo;
+        bool hit = Physics.Raycast(cam.ScreenPointToRay(Input.mousePosition), out hitInfo);
+        if (hit) // Show cursor when in bounds
         {
-            RaycastHit hit;
-            if (Physics.Raycast(cam.ScreenPointToRay(Input.mousePosition), out hit))
-            {
-                if (target)
-                    toDestroy.Add(target.gameObject);
-                target = Instantiate(selectionPrefab, hit.transform).GetComponent<Transform>();
-                target.position = hit.point;
-            }
+            cursor.gameObject.SetActive(true);
+            cursor.position = hitInfo.point;
         }
-    }
-
-    private void Clean()
-    {
-        foreach (GameObject obj in toDestroy)
-            Destroy(obj);
+        else // Remove the cursor when out of bounds
+        {
+            cursor.gameObject.SetActive(false);
+        }
     }
 }
