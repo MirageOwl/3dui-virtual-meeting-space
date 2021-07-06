8a072ba64a9248f9574c28e66110d1e91783ec54
doga <ydoga123@gmail.com>
2021-07-06 22:24:47 +0200

    Checkpoints only activate for objects with the Player tag

--- Assets/Scripts/OnTriggerCheckpoint.cs
+++ Assets/Scripts/OnTriggerCheckpoint.cs
@@ -6,12 +6,15 @@ public class OnTriggerCheckpoint : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        //This has runtime of like O(fucking long), but I don't care
-        var loggerGO = GameObject.Find("Logger");
-        var logger = (Logger) loggerGO.GetComponent(typeof(Logger));
-        logger.EnterCheckpoint();
-        isDisabled = true;
-        CheckpointOrder.Instance.Advance();
-        this.gameObject.SetActive(false);
+        if (other.CompareTag("Player"))
+        {
+            //This has runtime of like O(fucking long), but I don't care
+            var loggerGO = GameObject.Find("Logger");
+            var logger = (Logger) loggerGO.GetComponent(typeof(Logger));
+            logger.EnterCheckpoint();
+            isDisabled = true;
+            CheckpointOrder.Instance.Advance();
+            this.gameObject.SetActive(false);
+        }
     }
 }
